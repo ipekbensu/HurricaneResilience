9ec3ee1af2b78394e2e3d554bf838236c26c48a7
ipekbensu <ibmanav@gmail.com>
2019-05-06 04:54:48 -0400

    added AmplifiedMorbidities/dataBuilt.js

---
+++ AmplifiedMorbidities/dataBuilt.js
@@ -0,0 +1,71 @@
+var csv3 = `County,FIPS code,Rural/Urban,Old/New
+Alachua County,"�""12001""",�2,�2
+Baker County,"�""12003""",�3,�2
+Bay County,"�""12005""",�1,�2
+Bradford County,"�""12007""",�0,�2
+Brevard County,"�""12009""",�2,�2
+Broward County,"�""12011""",�3,�2
+Calhoun County,"�""12013""",�0,�2
+Charlotte County,"�""12015""",�1,�1
+Citrus County,"�""12017""",�1,�1
+Clay County,"�""12019""",�3,�1
+Collier County,"�""12021""",�2,�1
+Columbia County,"�""12023""",�0,�2
+DeSoto County,"�""12027""",�0,�2
+Dixie County,"�""12029""",�0,�2
+Duval County,"�""12031""",�3,�2
+Escambia County,"�""12033""",�2,�2
+Flagler County,"�""12035""",�2,�1
+Franklin County,"�""12037""",�0,�2
+Gadsden County,"�""12039""",�2,�2
+Gilchrist County,"�""12041""",�2,�1
+Glades County,"�""12043""",�0,�2
+Gulf County,"�""12045""",�1,�2
+Hamilton County,"�""12047""",�0,�2
+Hardee County,"�""12049""",�0,�2
+Hendry County,"�""12051""",�0,�2
+Hernando County,"�""12053""",�3,�1
+Highlands County,"�""12055""",�1,�2
+Hillsborough County,"�""12057""",�3,�2
+Holmes County,"�""12059""",�0,�2
+Indian River County,"�""12061""",�1,�1
+Jackson County,"�""12063""",�0,�2
+Jefferson County,"�""12065""",�2,�2
+Lafayette County,"�""12067""",�0,�2
+Lake County,"�""12069""",�3,�1
+Lee County,"�""12071""",�2,�1
+Leon County,"�""12073""",�2,�2
+Levy County,"�""12075""",�0,�2
+Liberty County,"�""12077""",�0,�2
+Madison County,"�""12079""",�0,�2
+Manatee County,"�""12081""",�2,�2
+Marion County,"�""12083""",�2,�1
+Martin County,"�""12085""",�2,�2
+Miami-Dade County,"�""12086""",�3,�2
+Monroe County,"�""12087""",�0,�2
+Nassau County,"�""12089""",�3,�2
+Okaloosa County,"�""12091""",�1,�2
+Okeechobee County,"�""12093""",�0,�2
+Orange County,"�""12095""",�3,�1
+Osceola County,"�""12097""",�3,�1
+Palm Beach County,"�""12099""",�3,�2
+Pasco County,"�""12101""",�3,�1
+Pinellas County,"�""12103""",�3,�3
+Polk County,"�""12105""",�2,�2
+Putnam County,"�""12107""",�0,�2
+St. Johns County,"�""12109""",�3,�1
+St. Lucie County,"�""12111""",�2,�1
+Santa Rosa County,"�""12113""",�2,�1
+Sarasota County,"�""12115""",�2,�2
+Seminole County,"�""12117""",�3,�2
+Sumter County,"�""12119""",�1,�1
+Suwannee County,"�""12121""",�0,�2
+Taylor County,"�""12123""",�0,�2
+Union County,"�""12125""",�0,�1
+Volusia County,"�""12127""",�2,�2
+Wakulla County,"�""12129""",�2,�0
+Walton County,"�""12131""",�1,�0
+Washington County,"�""12133""",�0,�0`;
+
+// debugging in node
+// module.exports = csv3;
